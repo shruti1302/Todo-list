b6060ef10accced4ba635f4fe02288fefa909ee3
Shruti <agarwal.shruti1302@gmail.com>
2020-05-02 17:25:17 +0530

    Initial commit

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,36 @@
+// import React, { useState } from "react";
+
+// function ToDoItem(props) {
+//   const [isDone, setIsDone] = useState(false);
+
+//   function handleClick() {
+//     setIsDone(prevValue => {
+//       return !prevValue;
+//     });
+//   }
+//   return (
+//     <div onClick={handleClick}>
+//       <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+//         {props.text}
+//       </li>
+//     </div>
+//   );
+// }
+
+// export default ToDoItem;
+
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
+}
+
+export default ToDoItem;
